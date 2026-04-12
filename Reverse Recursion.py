aa2b9967feb5929ab72bf92731e2b077372f9a4b
NikitaKleymenov <nikita.kleymenov2006@yandex.ru>
2026-04-13 02:13:43 +0300

    Update Reverse Recursion.py

--- Reverse Recursion.py
+++ Reverse Recursion.py
@@ -2,7 +2,6 @@ i = [15, 7, 22, 4, 16]
 
 def sum_rev(arr, idx, total):
     if idx == len(arr):
-        print(f'шаг суммы {idx+1}: total = {total}')
         return total
     total = total + arr[idx]
     print(f'шаг суммы {idx+1}: total = {total}')
@@ -10,7 +9,7 @@ def sum_rev(arr, idx, total):
 
 
 def max_rev(arr, idx, cur_max):
-    print(f'шаг максимума {idx+1}: cur_max = {cur_max}')
+    print(f'шаг максимума {idx}: cur_max = {cur_max}')
     if idx == len(arr):
         return cur_max
     if arr[idx] > cur_max:
@@ -18,16 +17,16 @@ def max_rev(arr, idx, cur_max):
     return max_rev(arr, idx + 1, cur_max)
 
 def min_rev(arr, idx, cur_min):
-    print(f'шаг минимума {idx+1}: cur_min = {cur_min}')
+    print(f'шаг минимума {idx}: cur_min = {cur_min}')
     if idx == len(arr):
         return cur_min
     if arr[idx] < cur_min:
         cur_min = arr[idx]
     return min_rev(arr, idx + 1, cur_min)
 
-mn = min_rev(i, 0, i[0])
+mn = min_rev(i, 1, i[0])
 print()
-mx = max_rev(i, 0, i[0])
+mx = max_rev(i, 1, i[0])
 print()
 sm = sum_rev(i, 0, 0)
 
